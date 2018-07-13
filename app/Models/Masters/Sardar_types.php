8e43fdbb3d3323509cbbf67526da1fcffbb4cb2b
rituraj09 <rituraj.b@corp.csmpl.com>
2018-07-13 10:24:14 +0530

    migrate db

---
+++ app/Models/Masters/Sardar_types.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Masters;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Sardar_types extends Model
+{
+    //
+}
---
+++ database/migrations/2018_07_12_155942_create_sardars_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSardarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sardars', function (Blueprint $table) {
+            $table->increments('id');            
+            $table->string('name');        
+            $table->string('phn');     
+            $table->integer('sardar_type_id', false, true);
+            $table->integer('mill_id', false, true);
+            $table->boolean('status')->default(1); 
+            $table->integer('created_by');
+            $table->integer('updated_by');  
+            $table->timestamps();
+            $table->foreign('sardar_type_id')->references('id')->on('sardar_types');
+            $table->foreign('mill_id')->references('id')->on('mills');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sardars');
+    }
+}
