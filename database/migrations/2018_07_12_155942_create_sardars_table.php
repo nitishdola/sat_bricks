e5847650876590e3f7cbac4320261f8de8c5352c
nitish dolakasharia <nitish.dola@gmail.com>
2018-07-13 10:27:21 +0530

    added field address(nullbale) in sardars table, renamed phn to mobile_numer

--- database/migrations/2018_07_12_155942_create_sardars_table.php
+++ database/migrations/2018_07_12_155942_create_sardars_table.php
@@ -16,12 +16,11 @@ class CreateSardarsTable extends Migration
         Schema::create('sardars', function (Blueprint $table) {
             $table->increments('id');            
             $table->string('name');        
-            $table->string('phn');     
+            $table->string('mobile_number');
+            $table->string('address')->nullable();
             $table->integer('sardar_type_id', false, true);
             $table->integer('mill_id', false, true);
             $table->boolean('status')->default(1); 
-            $table->integer('created_by');
-            $table->integer('updated_by');  
             $table->timestamps();
             $table->foreign('sardar_type_id')->references('id')->on('sardar_types');
             $table->foreign('mill_id')->references('id')->on('mills');
